a8bf25ae6650c310ed990bc9458cd547fb165fe8
Fedor Sumkin <qosys.net@gmail.com>
2012-10-31 02:05:24 +0400

    changed styles, rules on main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def mark_required(object, attribute)
     "*" if object.class.validators_on(attribute).map(&:class).include? ActiveModel::Validations::PresenceValidator
   end
+
+  def registration_text
+    return t(:registration) if Settings.registration_opened
+    return t(:request_invite)
+  end
 end
